a18bbe710a19d848f9f141f1c7e1ac4c1d8d8709
Grin <grinefimov@yandex.ru>
2019-07-07 23:56:24 +0300

    Layout, Users Database
    Add Home page, Bootstrap 4, Layout (Navbar, Sticky Footer), Cookie Policy, Exception Handler, Users Database - MVC Controller with views, using Entity Framework.

---
+++ GrinWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GrinWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
